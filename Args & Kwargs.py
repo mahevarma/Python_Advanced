4b80dceac28a0a8984e72a504396a39d696f4a58
Mahesh Varma <maheshgmv@gmail.com>
2021-02-02 13:02:12 +0530

    Added Kwags example

--- Args & Kwargs.py
+++ Args & Kwargs.py
@@ -49,3 +49,14 @@ def func(x, *args, **kwargs):
 
 
 func('Mahesh', 'name1', 'name2', name='varma')
+
+
+def concatenate(**kwargs):
+    result = ""
+    # Iterating over the Python kwargs dictionary
+    for arg in kwargs.values():
+        result += arg
+    return result
+
+
+print(concatenate(a="Real", b="Python", c="Is", d="Great", e="!"))
